a4041ef287acfa118db71373b0ed2ae977bdae30
Dan Petrisko <petrisko@cs.washington.edu>
2022-02-23 07:13:24 -0800

    Update bsg_clk_gen_monitor_clk_buf.v

--- bsg_clk_gen_pearl/bsg_clk_gen_monitor_clk_buf.v
+++ bsg_clk_gen_pearl/bsg_clk_gen_monitor_clk_buf.v
@@ -1,6 +1,6 @@
 
 // replace with hardened buffer with decent drive strength
-module bsg_clk_gen_monitor_clk_buf
+module bsg_clk_gen_pearl_monitor_clk_buf
   (input          i
    , output logic o
    );
