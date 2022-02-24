4ee2e98415bdd21157d3e1f9d3449ed609b2a3f7
Dan Petrisko <petrisko@cs.washington.edu>
2022-02-24 04:03:11 -0800

    removing clock gen pearl reference

--- bsg_dmc_pearl/bsg_dmc_pearl_pkg.v
+++ bsg_dmc_pearl/bsg_dmc_pearl_pkg.v
@@ -3,13 +3,12 @@ package bsg_dmc_pearl_pkg;
 
   import bsg_tag_pkg::*;
   import bsg_dmc_pkg::*;
-  import bsg_clk_gen_pearl_pkg::*;
 
   typedef struct packed
   {
     bsg_tag_s                     monitor_ds;
     bsg_dmc_osc_tag_lines_s       osc;
-    bsg_dmc_cfg_tag_lines_s       sys;
+    bsg_dmc_sys_tag_lines_s       sys;
     bsg_dmc_cfg_tag_lines_s       cfg;
     bsg_dmc_dly_tag_lines_s       dly;
   }  bsg_dmc_pearl_tag_lines_s;
